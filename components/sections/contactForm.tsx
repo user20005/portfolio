9d439c327b22820737d32ab11494baaa404e6e65
Marzouk <nemendmd@gmail.com>
2026-01-18 14:14:38 +0100

    Correction

--- components/sections/contactForm.tsx
+++ components/sections/contactForm.tsx
@@ -35,7 +35,7 @@ export const ContactForm = () => {
     setError(null);
 
     try {
-      const response = await fetch("/portfolio/api/send", {
+      const response = await fetch("/api/send", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
